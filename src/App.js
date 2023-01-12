7250a0f70e0a70ffcbd175e3e54945e54b7c61d7
Manoj Chauhan <an.ambitious.techie@gmail.com>
2023-01-13 00:00:25 +0530

    page background changed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         Welcome to Samrish
-        
       </header>
+      ****  We are coming....  ****
     </div>
   );
 }
