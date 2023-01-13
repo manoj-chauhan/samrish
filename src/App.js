ce207659edc43114745e212596200bb597952380
Manoj Chauhan <manoj@ecomexpress.in>
2023-01-13 23:35:35 +0530

    icon issue fixed

--- src/App.js
+++ src/App.js
@@ -509,10 +509,9 @@ function App() {
       <section id="contact">
         <div class="container">
           <div class="contact-card">
-            <div class="infos">
+          <div class="infos">
               <h6 class="section-subtitle">Get Here</h6>
               <h6 class="section-title mb-4">Contact Us</h6>
-
               <div class="item">
                 <i class="ti-location-pin"></i>
                 <div class="">
@@ -542,26 +541,12 @@ function App() {
                 </div>
               </div>
             </div>
-            <div class="form">
-              <h6 class="section-subtitle">Available 24/7</h6>
-              <h6 class="section-title mb-4">Get In Touch</h6>
-              <form>
-                <div class="form-group">
-                  <input type="email" class="form-control form-control-lg" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" required></input>
-                </div>
-                <div class="form-group">
-                  <input type="password" class="form-control form-control-lg" id="exampleInputPassword1" placeholder="Password" required></input>
-                </div>
-                <div class="form-group">
-                  <textarea name="contact-message" id="" cols="30" rows="7" class="form-control form-control-lg" placeholder="Message"></textarea>
-                </div>
-                <button type="submit" class="btn btn-primary btn-block btn-lg mt-3">Send Message</button>
-              </form>
-            </div>
+            
           </div>
         </div>
       </section>
 
+      {/* Footer  */}
       <section class="has-bg-img py-0">
         <div class="container">
           <div class="footer">
