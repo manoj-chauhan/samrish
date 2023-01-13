20c80b5e3d29ebfc359ff78881fdb36c3679fb09
Manoj Chauhan <manoj@ecomexpress.in>
2023-01-13 18:09:12 +0530

    new design started

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Welcome to Samrish
+    <div className='app-main'>
+      <nav className='custom-navbar custom-navbar-affix'>
+      <div class="container navbar-container">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#home">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#about">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#service">Service</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#portfolio">Portfolio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#team">Team</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#testimonial">Testimonial</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#blog">Blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#contact">Contact</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link btn btn-primary btn-sm ml-lg-3" href="components.html">Components</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+      </nav>
+      <header className="header">
+        <div class="overlay">
+            <h1>Samrish</h1>
+        </div>
       </header>
-      ****  We are coming....  ****
     </div>
   );
 }
